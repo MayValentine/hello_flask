d67f449704133d8c5b990137b2e32be9df8c2fc2
May Jimenez <may_coloresjp@hotmail.com>
2022-09-15 13:59:29 +0200

    servidor montado

---
+++ hello.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/hola") #decorador. Va a hacer la funcion accesible desde el servidor
+def primeraentrada():
+    return "Hola, mundo"
+
+
+@app.route("/adios")
+def salida():
+    return "Hasta luego, Mari Carmen!!!"
+
+"""
+@app.route("/romano/<numero>")
+def romano(numero):
+    return "el numero romano de numero es " + str(numero)
+"""
+
+@app.route("/doble/<numero>") #si ponemos int:numero esta restringiendo a numeros int
+def doble(numero):
+    return str(numero * 2)
+
+
